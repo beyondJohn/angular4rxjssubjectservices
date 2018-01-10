f9e065a6976b2a7c513d9e83a0dfc756fca1c4fe
John Piesco <jp@Johns-iMac.local>
2018-01-10 15:07:45 -0500

    added redundant component to check cross connectivity of service, also tested moving service provider from app.module and app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UiComponent } from './ui/ui.component';
+import { AuthService } from '../app/services/auth.service';
+import { RedundantComponent } from './redundant/redundant.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UiComponent
+    UiComponent,
+    RedundantComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  //providers: [ AuthService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
